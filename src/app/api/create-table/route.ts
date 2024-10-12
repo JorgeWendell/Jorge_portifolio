c0b025d62fe1e30394de531fa6993fa4dc954e8a
JorgeWendell <jorge.wendell@adelbr.tech>
2024-10-12 03:19:50 -0300

    initial 7

--- src/app/api/create-table/route.ts
+++ src/app/api/create-table/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   try {
     const result =
-      await sql`CREATE TABLE PreEnrollment  ( Name varchar(255), Email varchar(255), Enrollment varchar(5), Phone varchar(25), Message varchar(255))`;
+      await sql`CREATE TABLE PreEnrollment  ( Name varchar(255), Email varchar(255), Phone varchar(25), Message varchar(255))`;
 
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
